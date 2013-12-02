28ef9d8f0919305dbc995942634b816f095d2892
Bert Frees <bert.frees@sbs.ch>
2013-12-02 19:08:26 +0100

    Emacs indentation settings for dtbook-2005-3-sbs-full.rng

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil
-  (compile-command . "mvn clean package")))
+  (compile-command . "mvn clean package"))
+ ("src/main/schema/dtbook-2005-3-sbs-full.rng"
+  . ((nil . ((indent-tabs-mode . t)
+             (tab-width . 4)
+             (nxml-child-indent . 4))))))
